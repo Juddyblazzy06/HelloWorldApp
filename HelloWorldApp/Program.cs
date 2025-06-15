102cd8f0f487d9c22c9847018ccbaf481bef7cbf
amien <amien@DESKTOP-0F2452Q>
2025-06-16 00:22:46 +0100

    Add project files.

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello, World from Jude Amiens!");
+        }
+    }
+}
